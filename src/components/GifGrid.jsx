01fd89c8bcdc4dc13e31a13de79dd9b608203460
Samuel Cárdenas <samuelcardenas1411@gmail.com>
2022-01-31 21:10:41 -0300

    First Commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,29 @@
+import { GifGridItem } from '../components/GifGridItem'
+
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({ category }) => {
+
+
+    const { data: images, loading } = useFetchGifs( category );
+  
+    
+
+
+  return (
+        <>
+            <h3 className='animate_animated animate__fadeIn'>{ category }</h3>
+            { loading && <p className='animate_animated animate__flash'>Loading..</p>}
+            <div className='card-grid'>
+                {
+                        images.map( img =>
+                            <GifGridItem
+                            key={ img.id }
+                            { ...img } 
+                            />
+                        )
+                }  
+        </div>
+        </>
+  )
+};
